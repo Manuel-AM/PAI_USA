2efd43f7e72c81478f716bde637a6a9fb2491c18
Sofgra <ferm20171@gmail.com>
2023-10-10 11:53:06 -0500

    Add -> New Scaffolded Item. In the Add Scaffold dialog, select Identity and click Add. In the Add Identity dialog, select the Account/Login page to override. Select your data context class (which should be pre-filled for you). Click Add. This will generate a default login page that you can customize according to your needs

---
+++ PAI_USA/Areas/Identity/Data/PAI_USAContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PAI_USA.Models;
+
+namespace PAI_USA.Data;
+
+public class PAI_USAContext : IdentityDbContext<IdentityUser>
+{
+    public PAI_USAContext(DbContextOptions<PAI_USAContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<CrmLogIn> CrmLogIn { get; set; }   
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- PAI_USA/Program.cs
+++ PAI_USA/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PAI_USA.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PAI_USAContextConnection") ?? throw new InvalidOperationException("Connection string 'PAI_USAContextConnection' not found.");
+
+builder.Services.AddDbContext<PAI_USAContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PAI_USAContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
